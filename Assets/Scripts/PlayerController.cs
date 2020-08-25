e884c385b8729d1cc43ec16a65b97ed07ca4d931
AngekNg <angekng@outlook.com>
2020-08-24 20:38:46 -0500

    Agregar personaje
    Se agregó personaje con movimiento.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public int velocidad;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Movimiento();
+        Debug.Log(Input.GetAxis("Horizontal"));
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    void Movimiento()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*velocidad, Input.GetAxisRaw("Vertical")*velocidad);
+    }
+}
