f4c69d9673d14e0ac7afc8c0497243637650f58f
AngekNg <angekng@outlook.com>
2020-11-02 15:50:22 -0600

    no c que estoy comitenado xd

---
+++ Assets/Scripts/FirewallController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirewallController : MonoBehaviour
+{
+    public GameObject[] objetosDesconocidos;
+
+    void Start()
+    {
+        foreach(GameObject x in objetosDesconocidos)
+        {
+            GetComponent<GameObject>();
+        }
+    }
+
+    void Update()
+    {
+        Debug.Log("A: "+ objetosDesconocidos[0].name);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region variables generales
     Rigidbody2D rb;
     public int velocidad;
+    #endregion
+
+    #region Datos de Absorción
+    public float rangoAbsorción;
+    #endregion
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -14,7 +21,8 @@ public class PlayerController : MonoBehaviour
     private void Update()
     {
         Movimiento();
-        Debug.Log(Input.GetAxis("Horizontal"));
+        if(Input.GetKey(KeyCode.Z))
+        Absorción();
     }
 
     void FixedUpdate()
@@ -26,4 +34,16 @@ public class PlayerController : MonoBehaviour
     {
         rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal")*velocidad, Input.GetAxisRaw("Vertical")*velocidad);
     }
+
+    void Absorción()
+    {
+        RaycastHit2D colisiónAbsorbción = Physics2D.CircleCast(transform.position, rangoAbsorción, Vector2.zero);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, rangoAbsorción);
+    }
+
 }
